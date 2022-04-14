0039cfedb29071d8f0f89721f1281653a76ca316
n0365359 <michael.blydenburgh@libertymutual.com>
2022-04-14 09:58:46 -0400

    fix response logic for add user

--- src/handlers/addUser.go
+++ src/handlers/addUser.go
@@ -11,7 +11,7 @@ func AddUser(c *gin.Context) {
 	var newUser AddUserPayload
 	err := c.Bind(&newUser)
 
-	if err != nil {
+	if err == nil {
 		fmt.Printf("parsed payload: %+v\n", newUser)
 		c.JSON(http.StatusCreated, gin.H{"userId": "029381"})
 	}
