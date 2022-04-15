514e3bc44bdb9591875080fd3e7f920e9d6c262c
n0365359 <michael.blydenburgh@libertymutual.com>
2022-04-15 09:20:34 -0400

    remove index on scan

--- src/repository/repository.go
+++ src/repository/repository.go
@@ -53,7 +53,7 @@ func GetAllUsers() (*[]UserItem, error) {
 	var result []UserItem
 
 	rangeKey := "User#"
-	err := table.Scan().Index("modelTypeAndIdIndex").Filter("ModelTypeAndId", dynamo.BeginsWith, rangeKey).All(&result)
+	err := table.Scan().Filter("ModelTypeAndId", dynamo.BeginsWith, rangeKey).All(&result)
 	if err != nil {
 		log.Println("Error performing getItem operation")
 		log.Println(err)
